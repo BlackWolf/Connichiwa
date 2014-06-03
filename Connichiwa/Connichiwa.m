9b0278b55f2487803e61cf1c2a9dc1b497f8a54c
Mario Schreiner <BlackWolf@BlackBook-2.local>
2014-06-03 22:13:32 +0200

    Initial XCode project

---
+++ Connichiwa/Connichiwa.m
@@ -0,0 +1,13 @@
+//
+//  Connichiwa.m
+//  Connichiwa
+//
+//  Created by Mario Schreiner on 03/06/14.
+//  Copyright (c) 2014 Mario Schreiner. All rights reserved.
+//
+
+#import "Connichiwa.h"
+
+@implementation Connichiwa
+
+@end
---
+++ ConnichiwaTests/ConnichiwaTests.m
@@ -0,0 +1,34 @@
+//
+//  ConnichiwaTests.m
+//  ConnichiwaTests
+//
+//  Created by Mario Schreiner on 03/06/14.
+//  Copyright (c) 2014 Mario Schreiner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ConnichiwaTests : XCTestCase
+
+@end
+
+@implementation ConnichiwaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
