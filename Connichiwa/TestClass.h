006b09e71738ba167af2d9a2a23f930013e85fe6
Mario Schreiner <BlackWolf@BlackBook-2.local>
2014-06-04 11:37:56 +0200

    Added Fat Binary and Resources bundle to the framework

--- Connichiwa/TestClass.h
+++ Connichiwa/TestClass.h
@@ -1,11 +1,13 @@
 //
-//  Connichiwa.h
+//  TestClass.h
 //  Connichiwa
 //
-//  Created by Mario Schreiner on 03/06/14.
+//  Created by Mario Schreiner on 04/06/14.
 //  Copyright (c) 2014 Mario Schreiner. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-#import <Connichiwa/TestClass.h>
+@interface TestClass : NSObject
+
+@end
---
+++ Connichiwa/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Connichiwa
+//
+//  Created by Mario Schreiner on 04/06/14.
+//  Copyright (c) 2014 Mario Schreiner. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
