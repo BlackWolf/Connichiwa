6ba312db256be4fcac5ea293b881a21cc99c09ec
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2014-06-23 10:15:41 +0200

    Added remote webpage
    Added webpage that is opened by remote devices and the first draft of the remote library

---
+++ ConnichiwaResources/remote/lib.js
@@ -0,0 +1,102 @@
+"use strict";
+
+
+
+//Parse URL
+var ParsedURL = (function()
+{
+  var parser = document.createElement("a");
+  parser.href = document.URL;
+  
+  return parser;
+})();
+
+
+
+var QueryString = (function() 
+{
+  var _queryString = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    // If first entry with this name
+    if (typeof _queryString[pair[0]] === "undefined") 
+    {
+      _queryString[pair[0]] = pair[1];
+    // If second entry with this name
+    } 
+    else if (typeof _queryString[pair[0]] === "string") 
+    {
+      var arr = [ _queryString[pair[0]], pair[1] ];
+      _queryString[pair[0]] = arr;
+      // If third or later entry with this name
+    } 
+    else 
+    {
+      _queryString[pair[0]].push(pair[1]);
+    }
+  }
+   
+  return _queryString;
+})();
+
+
+var websocket = new WebSocket("ws://" + ParsedURL.hostname + ":" + (parseInt(ParsedURL.port) + 1));
+alert("Connecting to ws://" + ParsedURL.hostname + ":" + (parseInt(ParsedURL.port) + 1));
+
+websocket.onopen = function()
+{
+  // var data = { type: "remoteidentifier", identifier: QueryString.identifier };
+  // websocket.send(JSON.stringify(data));
+};
+
+
+websocket.onmessage = function(e)
+{
+  var message = e.data;
+  log("message: " + message);
+};
+
+
+websocket.onerror = function()
+{
+  alert("errored");
+};
+
+
+websocket.onclose = function()
+{
+  alert("closed");
+};
+
+
+//////////
+// MISC //
+//////////
+
+
+function log(message)
+{
+  console.log(getDateString() + " -- " + message);
+}
+
+function getDateString(date)
+{
+  if (date === undefined) date = new Date();
+
+  var hours = String(date.getHours());
+  hours = (hours.length === 1) ? "0" + hours : hours;
+
+  var minutes = String(date.getMinutes());
+  minutes = (minutes.length === 1) ? "0" + minutes : minutes;
+
+  var seconds = String(date.getSeconds());
+  seconds = (seconds.length === 1) ? "0" + seconds : seconds;
+
+  var milliseconds = String(date.getMilliseconds());
+  milliseconds = (milliseconds.length === 1) ? "00" + milliseconds : milliseconds;
+  milliseconds = (milliseconds.length === 2) ? "0" + milliseconds : milliseconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
