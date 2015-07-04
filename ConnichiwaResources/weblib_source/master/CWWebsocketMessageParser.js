35fb51d73a1a5fe22d982a44bfe1b4b4df747145
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2015-07-04 14:30:42 +0200

    Improved file autoload
    - autoload doesn't listen to only the last file's status anymore, it waits for all JS files to load and execute before reporting a success
    - autoload will now also wait for CSS files to load

--- ConnichiwaResources/weblib_source/master/CWWebsocketMessageParser.js
+++ ConnichiwaResources/weblib_source/master/CWWebsocketMessageParser.js
@@ -41,20 +41,19 @@ CWWebsocketMessageParser._parseRemoteInfo = function(message) {
   //If we have a non-native remote no device might exist since
   //no info was sent via BT. If so, create one now.
   if (device === null) {
-    device = new CWDevice(message); 
+    device = new CWDevice(message);
     CWDeviceManager.addDevice(device);
   } else {
     //TODO although unnecessary, for cleanness sake we should probably
     //overwrite any existing device data with the newly received data?
     //If a device exists, that data should be the same as the one we received
     //via BT anyways, so it shouldn't matter
-    CWDebug.log(1, 'TODO');
   }
-  
+
   device._connectionState = CWDevice.ConnectionState.CONNECTED;
   CWNativeBridge.callOnNative('nativeCallRemoteDidConnect', device.getIdentifier());
   // nativeCallRemoteDidConnect(device.getIdentifier());
-  
+
   //We want to do 3 things before we trigger the 'deviceConnected' event:
   //* Load the debug info on the other device
   //* Sync the CWDatastore to the other device
@@ -63,17 +62,17 @@ CWWebsocketMessageParser._parseRemoteInfo = function(message) {
   var didLoadData  = false;
   var didLoadFiles = false;
 
+  var tryDidConnectCallback = function() {
+    if (didLoadDebug === false || didLoadData === false || didLoadFiles === false) return;
+    CWEventManager.trigger('deviceConnected', device);
+    deferred.resolve();
+  };
+
   //LOAD DEBUG INFO
   device.send('_debuginfo', CWDebug._getDebugInfo(), function() {
     didLoadDebug = true;
     tryDidConnectCallback();
   });
-
-  var tryDidConnectCallback = function() { 
-    if (didLoadDebug === false || didLoadData === false || didLoadFiles === false) return;
-    CWEventManager.trigger('deviceConnected', device); 
-    deferred.resolve();
-  };
 
   //LOAD DATA
   CWDebug.log(3, 'Syncing entire datastore to connected device');
@@ -83,51 +82,34 @@ CWWebsocketMessageParser._parseRemoteInfo = function(message) {
   });
 
   //LOAD AUTOLOAD FILES
-  var loadOtherFile = function(device, file) {
-    //As of now, "other" files are only CSS
-    var extension = file.split('.').pop().toLowerCase();
-    if (extension === 'css') {
-      device.loadCSS(file);
-    } 
+  var requestedFiles = 0;
+  var resolvedFiles = 0;
+  var doneRequesting = false;
+
+  var fileWasResolved = function() {
+    resolvedFiles++;
+
+    if (doneRequesting && requestedFiles === resolvedFiles) {
+      didLoadFiles = true;
+      tryDidConnectCallback();
+    }
   };
-  var didLoadFilesCB = function() {
-    didLoadFiles = true;
-    tryDidConnectCallback();
-  };
-  
-  //We need to separate JS files from other filetypes in Connichiwa.autoLoad
-  //The reason is that we want to attach a callback to the last JS file we
-  //load, so we are informed when it was loaded. 
-  var autoLoadJS    = [];
-  var autoLoadOther = [];
+
   for (var i = 0; i < Connichiwa.autoLoad.length; i++) {
     var file = Connichiwa.autoLoad[i];
     var extension = file.split('.').pop().toLowerCase();
 
-    if (extension === 'js') autoLoadJS.push(file);
-    else autoLoadOther.push(file);
-  }
-
-  //First, let's load all non-JS files
-  for (var i = 0; i < autoLoadOther.length; i++) {
-    var file = autoLoadOther[i];
-    loadOtherFile(device, file);
-  }
-
-  //Now load all JS files and attach the callback to the last one
-  //If no JS files are auto-loaded, execute the callback immediately
-  if (autoLoadJS.length > 0) {
-    for (var i = 0; i < autoLoadJS.length; i++) {
-      var script = autoLoadJS[i];
-      if (i === (autoLoadJS.length - 1)) {
-        device.loadScript(script, didLoadFilesCB);
-      } else {
-        device.loadScript(script);
-      }
+    if (extension === "css") {
+      device.loadCSS(file, fileWasResolved);
+      requestedFiles++;
     }
-  } else {
-    didLoadFilesCB();
+
+    if (extension === "js") {
+      device.loadScript(file, fileWasResolved);
+      requestedFiles++;
+    }
   }
+  doneRequesting = true;
 
   return deferred;
 };
